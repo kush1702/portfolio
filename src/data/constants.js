fc879645d7a0a790f7e66f122a59633fee8413d4
kush <budhirajakush7@gmaill.com>
2024-07-07 00:55:14 +0530

    second commit

---
+++ src/data/constants.js
@@ -0,0 +1,190 @@
+export const Bio = {
+  name: "KUSH BUDHIRAJA",
+  roles: [
+    "Full Stack Developer",
+    "Song Writer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/kush1702",
+  resume:
+    "https://docs.google.com/document/d/1moUJd8OHUBmayctmw2s1bzz_nsfl8vHgkkh8esHwfnA/edit?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/kush-budhiraja-4a72a6212/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:"https://img.icons8.com/?size=100&id=kg46nzoJrmTR&format=png&color=000000"
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "DevOps & Fullstack Engineering Intern",
+    company: "Flipr Inovations Pvt. Ltd.",
+    date: "Aug 2023 - July 2023",
+    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+    skills: [
+      "Docker",
+      "Terraform",
+      "AWS",
+      "EC2",
+      "Portainer",
+      "Nginx",
+      "JavaScript",
+      "TypeScript",
+      "Node Js",
+      " Next Js",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+    role: "Frontend Engineer Intern",
+    company: "Neurobit",
+    date: "June 2023 - Present",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Fullstack Externship",
+    company: "Flipr",
+    date: "June 2023 - July 2023",
+    desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "GirlScript Summer of Code",
+    date: "May 2023 - Present",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+    doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+  },
+
+ 
+];
+
+
+export const projects = [
+  {
+    id: 0,
+    title: " Machine Learning-Powered Document Classification System",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Document Classifier with BERT Tokenization: Engineered a machine learning-driven classifier utilizing BERT for precise categorization across a diverse range of document types.Enhanced Classification Accuracy with Hugging Face Transfer Learning:Enhanced document classification accuracyby applying transfer learning with Hugging Face models, leveraging pre-trained transformers for improved performance.Efficient Interface for Dynamic Document Handling: Engineered a user-friendly interface using Streamlit, seamlessly integrating machine learning capabilities to classify various document types swiftly from PDF inputs.",
+    image:
+      "https://img.icons8.com/?size=100&id=sop9ROXku5bb&format=png&color=000000",
+    tags: ["HuggingFace", "Python", "Streamlit"],
+    category: "web app",
+    github: "https://github.com/kush1702/AKATSUKI-ADOBEDEVCRAFT",
+    webapp: "https://adobe-pvf3bbv6bsf.streamlit.app/",
+    member: [
+,
+    ],
+  },
+  {
+    id: 1,
+    title: " Real Estate content writer",
+    date: "jan 2024",
+    description:
+      "A Generative AI-powered tool designed to create compelling content for real estate properties. This application takes user inputs such as features, location, and property size utilizing large language models (LLM) to generate content suitable for property brochures. Front-End: Developed a React JS web app. User inputs are processed and sent as prompts to the OpenAI LLM model, which generates the copy. The output is received and displayed to the user. Back-End Integration:Developed a backend server using Express JS, featuring REST API endpoints to seamlessly interact with the AI language model , integrated Supabase for efficient storage of user inputs and generated outputs. Advanced Feature: Included a feature for users to selectively regenerate portions of the marketing copy, offering options to adjust the length, enhancing the flexibility and customization of the content.",
+    image:
+      "https://img.icons8.com/?size=100&id=FBO05Dys9QCg&format=png&color=000000",
+    tags: [
+      "React Js",
+      "SupaBase",
+      "Node Js",
+      "Express Js",
+      "Tailwind CSS",
+      "EDEN AI API"
+    ],
+    category: "web app",
+    github: "https://github.com/kush1702/Real_Estate_Marketing_brochure",
+    webapp: "https://photos.google.com/share/AF1QipN80JnRDt-uC42LXSVqU7QouPxO832raBUaL29mS37bGWJkkVI1f4BWnPfcbxstdA?key=NFNIVGhMeWVpb3ZpRkJlOG1zbG03N2UzVHZDTXJ3",
+  },
+];
+
+export const TimeLineData = [
+];
